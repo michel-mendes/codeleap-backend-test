204e5501faa71489fecc992389dd8ece4a1babf1
Michel Mendes <michel.gmsm@gmail.com>
2025-09-10 22:48:58 -0300

    chore: apply deploy configs

--- codeleap_backend_test/settings.py
+++ codeleap_backend_test/settings.py
@@ -22,12 +22,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kgnunu4)r=wwdzk*#)=v=k3700u^a=8k$@7iqj6o6n%-1+eztv'
+# SECRET_KEY = 'django-insecure-kgnunu4)r=wwdzk*#)=v=k3700u^a=8k$@7iqj6o6n%-1+eztv'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='this is my secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# DEBUG = False
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
@@ -97,7 +105,8 @@ WSGI_APPLICATION = 'codeleap_backend_test.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost:5432/codeleap_backend_test',
+        # default='postgresql://postgres:postgres@localhost:5432/codeleap_backend_test',
+        default = os.environ.get('DATABASE_URL'),
         conn_max_age=600
     )
     # 'default': {
